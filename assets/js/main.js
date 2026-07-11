5e638011e26c82d741d71ed6743dd412b6ac550a
EnriqueZS <enriquezozdesouza@gmail.com>
2026-07-10 23:01:51 -0300

    ctps funcionando

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,12 @@ const addButton = document.getElementById('add');
 const delButton = document.getElementById('del');
 const resetButton = document.getElementById('reset');
 const startButton = document.getElementById('start');
+const cpstButton = document.getElementById('cpsBtn');
 let result = document.getElementById('number');
+let cpsDisplay = document.getElementById('cpsNumber');
 
 let counter = parseInt(localStorage.getItem('myCounter')) || 0;
+let cpsClicks = 0;
 let colors = JSON.parse(localStorage.getItem('colors')) || {};
 let gameOn = false;
 
@@ -32,7 +35,17 @@ resetButton.addEventListener('click', () => {
 });
 
 startButton.addEventListener('click', () => {
+    cpsClicks = 0;
+    cpsDisplay.innerHTML = cpsClicks;
     countdown();
+});
+
+cpstButton.addEventListener('click', () => {
+    if (gameOn) {
+        cpsClicks++;
+        cpsDisplay.innerHTML = cpsClicks;
+        cpstButton.blur();
+    }
 });
 
 function update() {
@@ -63,6 +76,7 @@ function updateColor() {
 
 function countdown() {
     let sec = 3;
+    
     let timeCD = setInterval(function () {
         document.getElementById('realTimer').innerHTML = sec;
         sec--;
@@ -76,12 +90,23 @@ function countdown() {
 }
 
 function timer() {
-    let sec = 10;
+    gameOn = true;
+    cpstButton.textContent = '🟢';
+    let sec = 5;
     let timer = setInterval(function () {
         document.getElementById('realTimer').innerHTML = sec;
         sec--;
         if (sec < 0) {
             clearInterval(timer);
+            gameOn = false;
+            ClicksPerSec();
+            cpstButton.textContent = '🔴';
         }
     }, 1000);
+}
+
+function ClicksPerSec() {
+let speedResult;
+speedResult = cpsClicks / 5;
+cpsDisplay.innerHTML = speedResult;
 }
